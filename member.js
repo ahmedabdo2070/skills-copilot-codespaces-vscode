e4a29deccacafd202c25f327aa6b527aacc8bc00
Ahmed Abdo <ahmedabdo2070@gmail.com>
2023-07-21 16:11:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    if (memberValue == "member") {
+        memberSkills.style.display = "none";
+    } else if (memberValue == "member1") {
+        memberSkills.style.display = "block";
+        memberSkills.innerHTML = "<option value='member1Skills1'>Member 1 Skills 1</option><option value='member1Skills2'>Member 1 Skills 2</option><option value='member1Skills3'>Member 1 Skills 3</option><option value='member1Skills4'>Member 1 Skills 4</option><option value='member1Skills5'>Member 1 Skills 5</option><option value='member1Skills6'>Member 1 Skills 6</option><option value='member1Skills7'>Member 1 Skills 7</option><option value='member1Skills8'>Member 1 Skills 8</option><option value='member1Skills9'>Member 1 Skills 9
